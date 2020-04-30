a45ac93142a0de3504e1ba810c97ce9442f8e739
CJH254 <814626551@qq.com>
2020-04-30 21:09:17 +0800

    swagger api

--- src/main/java/gupt/cjh/noteblog/SwaggerConfig.java
+++ src/main/java/gupt/cjh/noteblog/SwaggerConfig.java
@@ -2,23 +2,22 @@ package gupt.cjh.noteblog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.w3c.dom.DocumentType;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @ClassName SwaggerConfig
- * @Description:
+ * @Description:Swagger api 接口文档
  * @Author 二维世界是个圆
  * @Date 2020/4/30
  * @Version V1.0
  **/
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
