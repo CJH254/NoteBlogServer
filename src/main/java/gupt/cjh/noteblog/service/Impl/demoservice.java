49065d5852afb8ce9fbfe261d4277b9da4f55ab8
CJH254 <814626551@qq.com>
2020-04-30 21:15:07 +0800

    demo

---
+++ src/main/java/gupt/cjh/noteblog/service/Impl/demoservice.java
@@ -0,0 +1,12 @@
+package gupt.cjh.noteblog.service.Impl;
+
+/**
+ * @ClassName demoservice
+ * @Description: TODO
+ * @Author 二维世界是个圆
+ * @Date 2020/4/30
+ * @Version V1.0
+ **/
+
+public class demoservice {
+}
